f1b2b5294e5773a00f7abf8fabdf1cb21c2926c8
Benja Aguilar <benjaaguilar02@gmail.com>
2025-01-13 09:38:29 -0300

    refactor: replace div with anchor and add elements

--- src/components/common/PostsCard.jsx
+++ src/components/common/PostsCard.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 function PostCard({
   id,
   title,
@@ -11,22 +9,31 @@ function PostCard({
   likes,
 }) {
   return (
-    <div>
-      <img src={thumbnail} alt={`thumbnail image of ${title}`} />
-      <p>{title}</p>
-      <div>
-        <p>views: {views}</p>
-        <p>likes: {likes}</p>
-        <p>comments: {comments}</p>
-        <p>readtime: {readtime} mins</p>
+    <a
+      href={`/posts/${id}`}
+      className="flex flex-col gap-3 rounded-lg border-cyan-500 border-2 flex-grow bg-white basis-1/2 md:basis-1/3 lg:basis-1/4"
+    >
+      <div className="flex flex-col gap-2 overflow-hidden rounded-lg w-full">
+        <img
+          src={thumbnail}
+          alt={`thumbnail image of ${title}`}
+          className="w-full h-auto object-cover"
+        />
+        <p className="w-full px-2">{title}</p>
       </div>
-      <div>
+      <div className="flex gap-3 w-full px-2">
         categories:
         {categories.map((category) => {
           return <p key={category.categoryId}>{category.categoryId}</p>;
         })}
       </div>
-    </div>
+      <div className="flex gap-3 px-2">
+        <p>{views}</p>
+        <p>{likes}</p>
+        <p>{comments}</p>
+        <p>{readtime} mins</p>
+      </div>
+    </a>
   );
 }
 
