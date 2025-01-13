18b3417f7c4bf3b816651120a576367f4e769f68
Benja Aguilar <benjaaguilar02@gmail.com>
2025-01-13 09:42:22 -0300

    style: add div and postsCard

--- src/components/common/PostsShowcase.jsx
+++ src/components/common/PostsShowcase.jsx
@@ -21,29 +21,31 @@ export function PostsShowcase() {
   console.log(errorMessage);
   console.log(posts);
   return (
-    <div>
+    <div className="p-4">
       <h2>Ultimos posts</h2>
-      {posts ? (
-        posts.map((post) => {
-          return (
-            <PostCard
-              id={post.id}
-              title={post.title}
-              thumbnail={post.thumbnailUrl}
-              views={post.views}
-              readtime={post.readtimeMin}
-              categories={post.categories}
-              comments={post.comments.length}
-              likes={post.userLikes.length}
-              key={post.id}
-            />
-          );
-        })
-      ) : errorMessage ? (
-        <div>{errorMessage}</div>
-      ) : (
-        <div>Loading...</div>
-      )}
+      <div className="flex flex-wrap gap-4">
+        {posts ? (
+          posts.map((post, i) => {
+            return (
+              <PostCard
+                id={post.id}
+                title={post.title}
+                thumbnail={post.thumbnailUrl}
+                views={post.views}
+                readtime={post.readtimeMin}
+                categories={post.categories}
+                comments={post.comments.length}
+                likes={post.userLikes.length}
+                key={post.id}
+              />
+            );
+          })
+        ) : errorMessage ? (
+          <div>{errorMessage}</div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     </div>
   );
 }
