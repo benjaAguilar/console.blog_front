7f9fb4ee34724644ed9d38117b86725c0317d413
Benja Aguilar <benjaaguilar02@gmail.com>
2025-01-23 10:13:40 -0300

    feat: add user data on comment

--- src/components/common/comments/GetComments.jsx
+++ src/components/common/comments/GetComments.jsx
@@ -1,17 +1,36 @@
+import { LikeButton } from "../LikeButton";
+
 export function GetComments({ comments }) {
   return (
-    <>
+    <div className="flex flex-col gap-4 mt-4 w-full">
       {comments ? (
         comments.map((comment, i) => {
+          const date = new Date(comment.createdAt).toDateString();
+
           return (
-            <div key={i}>
-              <p>{comment.content}</p>
+            <div key={i} className="w-full">
+              <hr />
+              <div className="p-4">
+                <div className="flex items-center justify-between gap-1 mb-2">
+                  <div className="flex items-center gap-1">
+                    <img
+                      src="/icons/user.svg"
+                      alt="user icon"
+                      className="w-7"
+                    />
+                    <p>{comment.owner.username}</p>
+                  </div>
+                  <i>{date}</i>
+                </div>
+                <p>{comment.content}</p>
+              </div>
+              <hr />
             </div>
           );
         })
       ) : (
         <div>Loading...</div>
       )}
-    </>
+    </div>
   );
 }
