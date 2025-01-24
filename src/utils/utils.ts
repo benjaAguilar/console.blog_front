d70fee734e684cb1e95a2f413b83d35dd5e94e66
Benja Aguilar <benjaaguilar02@gmail.com>
2025-01-24 15:29:38 -0300

    feat: add generic fetchData function this function ensures consistency on the fetchs response

---
+++ src/utils/utils.ts
@@ -0,0 +1,55 @@
+const server = import.meta.env.PUBLIC_SERVER;
+
+class CustomError extends Error {
+  details: Record<string, any>;
+
+  constructor(message: string, details: Record<string, any>) {
+    super(message);
+    this.name = "CustomError";
+    this.details = details;
+  }
+}
+
+export async function fetchData(
+  path: string,
+  method: string,
+  headers?: HeadersInit
+) {
+  try {
+    const response = await fetch(`${server}${path}`, {
+      method: method,
+      credentials: "include",
+      headers: headers,
+    });
+
+    if (response.status === 401) {
+      throw new CustomError(response.statusText, {
+        success: false,
+        message: response.statusText,
+      });
+    }
+
+    const data = await response.json();
+
+    if (!data.success) {
+      throw new CustomError(data.message, {
+        success: false,
+        message: data.message,
+        validationErrors: data.validationErrors || null,
+      });
+    }
+
+    return data;
+  } catch (error) {
+    if (error instanceof CustomError) {
+      console.error(error.message);
+      return error.details;
+    }
+
+    console.error("Unknown Error", error);
+    return {
+      success: false,
+      message: "An unknown error ocurred",
+    };
+  }
+}
