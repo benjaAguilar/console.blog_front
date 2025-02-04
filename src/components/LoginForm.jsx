640139d01ca4404d6b13c178064d64f0645eae95
Benja Aguilar <benjaaguilar02@gmail.com>
2025-02-04 12:57:55 -0300

    feat: add styles and input feedback

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,6 +6,8 @@ export function LoginForm() {
   const [errorMessages, setErrorMessages] = useState(null);
 
   const formRef = useRef();
+  const usernameRef = useRef();
+  const passwordRef = useRef();
 
   useEffect(() => {
     const successOnRegister = localStorage.getItem("successOnRegister");
@@ -21,6 +23,22 @@ export function LoginForm() {
       localStorage.removeItem("successOnRegister");
     }
   }, []);
+
+  function inputChange(e, min) {
+    console.log(e.target.value);
+    const regex = /^[A-Za-z0-9]+$/;
+
+    if (
+      e.target.value.length < min ||
+      e.target.value.length > 20 ||
+      !regex.test(e.target.value)
+    ) {
+      e.target.style.borderColor = "red";
+      return;
+    }
+
+    e.target.style.borderColor = "green";
+  }
 
   async function loginUser(e) {
     e.preventDefault();
@@ -43,6 +61,14 @@ export function LoginForm() {
     hasMessage.set(true);
 
     if (!loginData.success) {
+      loginData.validationErrors.forEach((err) => {
+        if (err.path === "username") {
+          usernameRef.current.style.borderColor = "red";
+        } else if (err.path === "password") {
+          passwordRef.current.style.borderColor = "red";
+        }
+      });
+
       return setErrorMessages(loginData.validationErrors);
     }
 
@@ -52,21 +78,61 @@ export function LoginForm() {
 
   return (
     <>
-      {errorMessages
-        ? errorMessages.map((message, i) => {
-            return <p key={i}>{message.msg}</p>;
-          })
-        : null}
-      <form id="formu" ref={formRef} className="flex flex-col gap-4">
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
-
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
+      {errorMessages && errorMessages.length > 0 && (
+        <div
+          className="p-4 mb-4 rounded-md bg-red"
+          style={{
+            backgroundColor: "#f87171",
+            backgroundImage: "var(--noise)",
+          }}
+        >
+          {errorMessages.map((message, i) => (
+            <p key={i} className="font-medium">
+              * {message.msg}
+            </p>
+          ))}
+        </div>
+      )}
+      <form
+        id="formu"
+        ref={formRef}
+        style={{
+          backgroundColor: "var(--col-black)",
+          backgroundImage: "var(--noise)",
+        }}
+        className="flex flex-col gap-4 p-4 text-[--col-white] rounded-xl"
+      >
+        <div className="flex flex-col gap-1">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            ref={usernameRef}
+            onChange={(e) => {
+              inputChange(e, 4);
+            }}
+            className="bg-transparent border-2 rounded-md p-1 focus-visible:outline-none"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            ref={passwordRef}
+            onChange={(e) => {
+              inputChange(e, 8);
+            }}
+            className="bg-transparent border-2 rounded-md p-1 focus-visible:outline-none"
+          />
+        </div>
 
         <input
           type="submit"
           value="Login"
+          className="bg-amber-500 p-2 text-[--col-black] border-amber-500 border-2 rounded"
           onClick={(e) => {
             loginUser(e);
           }}
