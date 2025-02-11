d253297abbd06ffbc2269e078e80b6fa3d0314ac
Benja Aguilar <benjaaguilar02@gmail.com>
2025-02-11 10:47:31 -0300

    feat: add custom animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        intermitentOpacity:
+          "intermitentOpacity 1s ease-in-out infinite alternate",
+      },
+      keyframes: {
+        intermitentOpacity: {
+          "0%": {
+            opacity: 0.3,
+          },
+          "100%": {
+            opacity: 0.6,
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
