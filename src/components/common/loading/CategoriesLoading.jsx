0b68e5ed2b10d90c0f589ee9f8ef44503fef45a2
Benja Aguilar <benjaaguilar02@gmail.com>
2025-02-12 10:40:28 -0300

    style: modified radius and height

--- src/components/common/loading/CategoriesLoading.jsx
+++ src/components/common/loading/CategoriesLoading.jsx
@@ -3,27 +3,27 @@ export function CategoriesLoading() {
     <div className="animate-intermitentOpacity">
       <nav className="flex items-center gap-4 w-full overflow-x-auto">
         <div className="border-[--col-black] border-2 rounded-2xl pl-4 pr-4 opacity-25">
-          <div className="w-11 h-3 rounded-md bg-[--col-black] my-1"></div>
+          <div className="w-11 h-2 rounded-sm bg-[--col-black] my-2"></div>
         </div>
 
         <div className="border-[--col-black] border-2 rounded-2xl pl-4 pr-4 opacity-25">
-          <div className="w-11 h-3 rounded-md bg-[--col-black] my-1"></div>
+          <div className="w-11 h-2 rounded-sm bg-[--col-black] my-2"></div>
         </div>
 
         <div className="border-[--col-black] border-2 rounded-2xl pl-4 pr-4 opacity-25">
-          <div className="w-11 h-3 rounded-md bg-[--col-black] my-1"></div>
+          <div className="w-11 h-2 rounded-sm bg-[--col-black] my-2"></div>
         </div>
 
         <div className="border-[--col-black] border-2 rounded-2xl pl-4 pr-4 opacity-25">
-          <div className="w-11 h-3 rounded-md bg-[--col-black] my-1"></div>
+          <div className="w-11 h-2 rounded-sm bg-[--col-black] my-2"></div>
         </div>
 
         <div className="border-[--col-black] border-2 rounded-2xl pl-4 pr-4 opacity-25">
-          <div className="w-11 h-3 rounded-md bg-[--col-black] my-1"></div>
+          <div className="w-11 h-2 rounded-sm bg-[--col-black] my-2"></div>
         </div>
 
         <div className="border-[--col-black] border-2 rounded-2xl pl-4 pr-4 opacity-25">
-          <div className="w-11 h-3 rounded-md bg-[--col-black] my-1"></div>
+          <div className="w-11 h-2 rounded-sm bg-[--col-black] my-2"></div>
         </div>
       </nav>
     </div>
